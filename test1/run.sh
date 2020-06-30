c676efbc5c42ce82aa2357fe41ce36a3f3e3b388
tonymmm1 <tony@mail.magnatox.com>
2020-06-30 01:25:05 -0500

    updated test1/run.sh with windows condition

--- test1/run.sh
+++ test1/run.sh
@@ -1,4 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH=$DIR/goworkspace # if it isn't already set
 # $GOPATH/bin/toml-test $GOPATH/bin/toml-test-decoder # e.g., run tests on my parser
-$GOPATH/bin/toml-test  ../build/toml_json
+if [ "$TRAVIS_OS_NAME" == "windows" ]
+then
+	$GOPATH/bin/toml-test  ../build/Release/toml_json.exe
+else
+	$GOPATH/bin/toml-test  ../build/toml_json
+fi
