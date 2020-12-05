5039045945827ec32cd7d61e08dfbd1f61986ca2
CK Tan <cktan@vitessedata.com>
2020-12-05 14:25:39 -0800

    minor

--- test1/run.sh
+++ test1/run.sh
@@ -1,4 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH=$DIR/goworkspace # if it isn't already set
+
 # $GOPATH/bin/toml-test $GOPATH/bin/toml-test-decoder # e.g., run tests on my parser
 $GOPATH/bin/toml-test  ../toml_json
